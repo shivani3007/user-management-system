e23389be5b6155141553ae63f9a353b3ddd5443a
shivani3007 <shivanichampaneria27@gmail.com>
2025-03-21 16:01:41 +0530

    Changes to migration file

--- src/migrations/1742453641212-roles.ts
+++ src/migrations/1742453641212-roles.ts
@@ -2,32 +2,39 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class Roles1742453641212 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
     const tableExists = await queryRunner.hasTable('roles');
 
-    if (!tableExists) {
-      await queryRunner.createTable(
-        new Table({
-          name: 'roles',
-          columns: [
-            {
-              name: 'id',
-              type: 'integer', // Change "number" to "integer"
-              isPrimary: true,
-              isGenerated: true,
-              generationStrategy: 'increment',
-            },
-            {
-              name: 'name',
-              type: 'varchar',
-              isUnique: true,
-            },
-          ],
-        }),
-      );
-    }
-    await queryRunner.query(`
+    try {
+      if (!tableExists) {
+        await queryRunner.createTable(
+          new Table({
+            name: 'roles',
+            columns: [
+              {
+                name: 'id',
+                type: 'integer', // Change "number" to "integer"
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: 'increment',
+              },
+              {
+                name: 'name',
+                type: 'varchar',
+                isUnique: true,
+              },
+            ],
+          }),
+        );
+      }
+      await queryRunner.query(`
             INSERT INTO roles (name) VALUES ('admin'), ('user')
           `);
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
